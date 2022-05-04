6581bd1a42d819389d3a186a4cdb718f12a5abd8
snoozers <49787185+snoozers@users.noreply.github.com>
2022-05-04 23:51:49 +0900

    clean: lineのapiへのリクエストにrequestsモジュールを使用するように

--- layer/linebot.py
+++ layer/linebot.py
@@ -1,23 +1,22 @@
 import os
 import json
-from urllib import request
+import requests
 
 def postMessage(message:str) -> None:
-    endpoint = 'https://api.line.me/v2/bot/message/push'
-    body = {
-        'to': os.environ['ADMIN_LINE_USER_ID'],
-        'messages': [{
-            'type': 'text',
-            'text': message
-        }]
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + os.environ['CHANNEL_ACCESS_TOKEN']
-    }
-
     try:
-        req = request.Request(endpoint, json.dumps(body).encode(), headers)
-        request.urlopen(req)
+        requests.post(
+            url='https://api.line.me/v2/bot/message/push',
+            data=json.dumps({
+                'to': os.environ['ADMIN_LINE_USER_ID'],
+                'messages': [{
+                    'type': 'text',
+                    'text': message
+                }]
+            }),
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + os.environ['CHANNEL_ACCESS_TOKEN']
+            }
+        )
     except Exception as e:
         print(e)
