8a0560ba82995df93fa093534240bcdede3c6b13
snoozers <49787185+snoozers@users.noreply.github.com>
2022-05-05 11:54:10 +0900

    add: 解錠時のスタンプを送信処理

--- layer/linebot.py
+++ layer/linebot.py
@@ -17,3 +17,27 @@ def postMessage(message:str) -> None:
             'Authorization': 'Bearer ' + os.environ['CHANNEL_ACCESS_TOKEN']
         }
     )
+
+def  post_unlocking_stamp() -> None:
+    _post_sticker(6359, 11069867)
+
+def _post_sticker(package_id:str, sticker_id:str) -> None:
+    """
+    See below for available parameters
+    https://developers.line.biz/ja/docs/messaging-api/sticker-list/
+    """
+    requests.post(
+        url='https://api.line.me/v2/bot/message/push',
+        data=json.dumps({
+            'to': os.environ['ADMIN_LINE_USER_ID'],
+            'messages': [{
+                'type': 'sticker',
+                'packageId': package_id,
+                'stickerId': sticker_id
+            }]
+        }),
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + os.environ['CHANNEL_ACCESS_TOKEN']
+        }
+    )
