d3444261480ff70f17d952f2d0818f12fd14e684
snoozers <49787185+snoozers@users.noreply.github.com>
2022-05-08 00:42:14 +0900

    add: カーテンを開く時刻を返却するように（テスト）

--- src/routine/morning/wake_up_time/app.py
+++ src/routine/morning/wake_up_time/app.py
@@ -1,3 +1,13 @@
-def lambda_handler(event, context):
+from datetime import datetime
+
+def lambda_handler(event, context) -> dict:
     # TODO: twitterから明日の起床時刻を取得
-    print('明日の起床時刻を取得する処理を実行')
+    now = datetime.utcnow()
+    print(now)
+    open_curtains_at = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+    print(event)
+    print(context)
+
+    return {
+        'open_curtains_at': open_curtains_at
+    }
