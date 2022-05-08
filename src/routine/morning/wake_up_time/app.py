7732011f4aad114f2a2af6510e7214116374635e
snoozers <49787185+snoozers@users.noreply.github.com>
2022-05-08 19:05:07 +0900

    add: カーテンを開く時刻を算出（テスト）

--- src/routine/morning/wake_up_time/app.py
+++ src/routine/morning/wake_up_time/app.py
@@ -1,13 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def lambda_handler(event, context) -> dict:
     # TODO: twitterから明日の起床時刻を取得
     now = datetime.utcnow()
-    print(now)
-    open_curtains_at = now.strftime('%Y-%m-%dT%H:%M:%SZ')
-    print(event)
-    print(context)
+    open_curtains_at = now + timedelta(seconds=60)
 
     return {
-        'open_curtains_at': open_curtains_at
+        'open_curtains_at': open_curtains_at.strftime('%Y-%m-%dT%H:%M:%SZ')
     }
