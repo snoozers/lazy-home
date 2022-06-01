966a3a4225e7f229df7b19b345285e708ae35ab2
snoozers <49787185+snoozers@users.noreply.github.com>
2022-06-01 20:08:08 +0900

    add: twitterのディスパッチャーにデバッグ用の処理追加

--- src/dispatcher/twitter/app.py
+++ src/dispatcher/twitter/app.py
@@ -1,8 +1,32 @@
+from datetime import datetime, timezone, timedelta
 from http import HTTPStatus
 import json
+import re
 
 def lambda_handler(event, context):
-    # TODO: ステートマシーンの実行を開始する処理など...
+    tweet = json.loads(event['body'])['tweet_create_events'][0]
+    print(tweet)
+
+    hashtags = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+    if '就寝' in hashtags:
+        # 設定した起床時刻のfromを取得する
+        jst_time = re.search(r'アラーム設定 (\d{1,2}:\d{2}) - \d{1,2}:\d{2}', tweet['text']).groups()[0]
+        jst_now = datetime.now(timezone(timedelta(hours=9)))
+        # 就寝時刻が午前の場合、起床日が同日になる
+        jst_date = jst_now.strftime('%Y-%m-%d') if jst_now.strftime('%p') == 'AM' else (jst_now + timedelta(days=1)).strftime('%Y-%m-%d')
+        # jst_open_curtains_at = datetime.strptime(jst_date + ' ' + jst_time, '%Y-%m-%d %H:%M:%SZ')
+        jst_datetime = datetime.strptime(jst_date + ' ' + jst_time + '+0900', '%Y-%m-%d %H:%M%z', )
+        # アラームが鳴る1分前にカーテンを開けたい
+        jst_open_curtains_at= jst_datetime + timedelta(minutes=-1)
+
+        utc_open_curtains_at = jst_open_curtains_at.astimezone(timezone.utc)
+
+        # StepFunctionsのWaitに指定可能なフォーマットに変更
+        res = utc_open_curtains_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+        print(jst_open_curtains_at)
+        print(utc_open_curtains_at)
+        print(res)
+
     action = 'test'
 
     return {
