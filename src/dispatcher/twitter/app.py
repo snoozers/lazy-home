8f1cda29970b73fd4e6e0dac2a4aea233261d638
snoozers <49787185+snoozers@users.noreply.github.com>
2022-06-01 21:31:56 +0900

    add: 実行中のステートマシーンがあれば停止して新たに実行

--- src/dispatcher/twitter/app.py
+++ src/dispatcher/twitter/app.py
@@ -6,39 +6,43 @@ import re
 from time import strftime
 import boto3
 
-def lambda_handler(event, context):
+def lambda_handler(event:dict, context:dict) -> dict:
     tweet = json.loads(event['body'])['tweet_create_events'][0]
-    print(tweet)
-
     hashtags = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+
     if '就寝' in hashtags:
-        # 設定した起床時刻のfromを取得する
-        jst_time = re.search(r'アラーム設定 (\d{1,2}:\d{2}) - \d{1,2}:\d{2}', tweet['text']).groups()[0]
-        jst_now = datetime.now(timezone(timedelta(hours=9)))
-        # 就寝時刻が午前の場合、起床日が同日になる
-        jst_date = jst_now.strftime('%Y-%m-%d') if jst_now.strftime('%p') == 'AM' else (jst_now + timedelta(days=1)).strftime('%Y-%m-%d')
-        jst_datetime = datetime.strptime(jst_date + ' ' + jst_time + '+0900', '%Y-%m-%d %H:%M%z', )
-        # アラームが鳴る1分前にカーテンを開けたい
-        jst_open_curtains_at= jst_datetime + timedelta(minutes=-1)
-        utc_open_curtains_at = jst_open_curtains_at.astimezone(timezone.utc)
-
-        print(jst_open_curtains_at)
-        print(utc_open_curtains_at)
-
-        client = boto3.client('stepfunctions')
-        client.start_execution(
-            stateMachineArn=os.environ['OPEN_CURTAINS_STATE_MACHINE_ARN'],
-            name=utc_open_curtains_at.strftime('%Y-%m-%d'),
-            # StepFunctionsのWaitに指定可能なフォーマットに変更
-            input=json.dumps(utc_open_curtains_at.strftime('%Y-%m-%dT%H:%M:%SZ'))
-        )
-
-
-    action = 'test'
+        execute_open_curtains_state_machine(get_utc_open_curtains_at(tweet))
 
     return {
-        'statusCode': HTTPStatus.OK,
-        'body': json.dumps({
-            'message': f'{action} was dispatched'
-        }),
+        'statusCode': HTTPStatus.OK
     }
+
+def get_utc_open_curtains_at(tweet:dict) -> datetime:
+    # 設定した起床時刻のfromを取得する
+    jst_time = re.search(r'アラーム設定 (\d{1,2}:\d{2}) - \d{1,2}:\d{2}', tweet['text']).groups()[0]
+    jst_now = datetime.now(timezone(timedelta(hours=9)))
+    # 就寝時刻が午前の場合、起床日が同日になる
+    jst_date = jst_now.strftime('%Y-%m-%d') if jst_now.strftime('%p') == 'AM' else (jst_now + timedelta(days=1)).strftime('%Y-%m-%d')
+    jst_datetime = datetime.strptime(jst_date + ' ' + jst_time + '+0900', '%Y-%m-%d %H:%M%z', )
+
+    # アラームが鳴る1分前にカーテンを開けたい
+    jst_open_curtains_at= jst_datetime + timedelta(minutes=-1)
+
+    return jst_open_curtains_at.astimezone(timezone.utc)
+
+def execute_open_curtains_state_machine(open_curtains_at: datetime) -> None:
+    client = boto3.client('stepfunctions')
+    # 実行中のOpenCurtainsステートマシーンがある場合停止して新たに実行
+    running = client.list_executions(
+        stateMachineArn=os.environ['OPEN_CURTAINS_STATE_MACHINE_ARN'],
+        statusFilter='RUNNING'
+    )
+    for execution in running['executions']:
+        client.stop_execution(executionArn=execution['executionArn'])
+    client.start_execution(
+        stateMachineArn=os.environ['OPEN_CURTAINS_STATE_MACHINE_ARN'],
+        # StepFunctionsのWaitに指定可能なフォーマットに変更
+        input=json.dumps({
+            'open_curtains_at': open_curtains_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+        })
+    )
--- src/routine/morning/open_curtains/app.py
+++ src/routine/morning/open_curtains/app.py
@@ -1,4 +1,7 @@
+import linebot
+
 def lambda_handler(event, context):
     # TODO: カーテンを開く処理
+    linebot.post_message('カーテンを開きました')
     print(event)
     print(context)
