5b8ced41d682de08d7ffa50620a0d07d08b05c08
snoozers <49787185+snoozers@users.noreply.github.com>
2022-10-11 18:42:57 +0900

    add: switchbotのwebhook取得処理追記

--- src/dispatcher/switch_bot/app.py
+++ src/dispatcher/switch_bot/app.py
@@ -1,8 +1,15 @@
 from http import HTTPStatus
 import json
+import linebot
 
 def lambda_handler(event:dict, context:dict) -> dict:
-    print(json.loads(event['body']))
+    # open | close | timeOutNotClose"
+    openState = json.loads(event['body'])['context']['openState']
+    if openState == 'open':
+        linebot.post_message('ドアが開きました')
+    elif openState == 'close':
+        linebot.post_message('ドアが閉まりました')
+
 
     return {
         'statusCode': HTTPStatus.OK
