dae78acbb8529097cc22eb204cbb907b9edf79ff
snoozers <49787185+snoozers@users.noreply.github.com>
2022-10-11 19:16:40 +0900

    add: ドアの開閉状態によって状態遷移するように

--- src/dispatcher/switch_bot/app.py
+++ src/dispatcher/switch_bot/app.py
@@ -1,16 +1,21 @@
 from http import HTTPStatus
 import json
+import time
+import boto3
 import linebot
 
-def lambda_handler(event:dict, context:dict) -> dict:
+def lambda_handler(event:dict, context:dict) -> None:
     # open | close | timeOutNotClose"
     openState = json.loads(event['body'])['context']['openState']
-    if openState == 'open':
-        linebot.post_message('ドアが開きました')
-    elif openState == 'close':
-        linebot.post_message('ドアが閉まりました')
-
-
-    return {
-        'statusCode': HTTPStatus.OK
-    }
+    client = boto3.client('iotevents-data')
+    if openState in ['open', 'close']:
+        res = client.batch_put_message(
+            messages=[{
+                'messageId': str(int(time.time())),
+                'inputName': 'front_door_input',
+                'payload': bytes(json.dumps({"door": {"event": openState}}), 'utf-8'),
+                'timestamp': {'timeInMillis': int(time.time())}
+            }]
+        )
+        if res.status_code != HTTPStatus.OK:
+            linebot.post_message('batch_put_messageの操作に失敗')
