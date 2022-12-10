85f7bceaf2a79c124c70c010140e80bcc4a153f5
snoozers <49787185+snoozers@users.noreply.github.com>
2022-12-11 02:52:01 +0900

    clean: 自動施錠時のLINEメッセージを削除

--- src/routine/auto_lock/lock_sesame/app.py
+++ src/routine/auto_lock/lock_sesame/app.py
@@ -1,5 +1,4 @@
 from sesame import Key
-import linebot
 
 def lambda_handler(event, context):
-    linebot.post_message('自動施錠しました') if Key().lock() else linebot.post_message('鍵の施錠に失敗しました')
+    Key().lock()
